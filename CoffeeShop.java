4f055c581b048c5532b52d3888a85ecd0a58578b
oseghale <ebeaemmanuel@gmail.com>
2022-12-26 07:27:16 +0000

    practice code

---
+++ CoffeeShop.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class CoffeeShop{
+    public static void main(String[] args){
+        while(true){
+        Scanner sc = new Scanner(System.in);
+        // ask for user balance
+        // ask user for coffee type
+        // ask user for quantity
+        // if user inputs 0 or negative number, produce no output
+        // the user should get a different message depending on what type of coffee purchsed
+
+        System.out.println("what is your balance");
+        String balanceInput = sc.nextLine();
+        double balance = Double.parseDouble(balanceInput);
+        System.out.println("what type of coffee do you want?");
+        String coffeeInput = sc.nextLine();
+        System.out.println("How many do you want?");
+        int qtyInput = Integer.parseInt(sc.nextLine());
+        double expressoPrice = 3.99;
+        double americanoPrice = 4.99;
+
+        if(qtyInput > 0){
+            //proceed with coffee purchase
+            boolean isExpresso = coffeeInput.equalsIgnoreCase("expresso");
+            boolean isAmericano = coffeeInput.equalsIgnoreCase("americano");
+            if(isExpresso && (balance >= (expressoPrice * qtyInput))){
+                // print out a successful transaction message
+                System.out.println("enjoy your expresso");
+            }
+            if(isAmericano && (balance >= (americanoPrice * qtyInput))){
+                 // print out a successful transaction message
+                 System.out.println("enjoy your americano");
+
+            }
+        }else{
+            System.out.println("no coffee - amount ordered is 0 or less");
+        }
+        // no coffee
+        
+
+    }
+    
+}
+
+}
---
+++ Hello.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Hello{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please input a number: ");
+        int number = sc.nextInt();
+        if(number >= 0){
+            System.out.println("Your input is positive! ");
+        }else if(number < 0){
+            System.out.println("Your input is negative!");
+        }
+        
+        System.out.println("Your input is " + number);
+        sc.close();
+
+    }
+    
+}
